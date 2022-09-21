ae1d63e39f27624bd46eab9bf492daf01a327b7e
itaintkevin <kpjtofficial@gmail.com>
2022-09-21 12:58:07 +0530

    Updated Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'custom-img':"url('/src/background.svg')",
+      }
+    },
   },
   plugins: [],
 }
